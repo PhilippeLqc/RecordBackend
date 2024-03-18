66b85b7dd0879a3bc5d36cef0472d59242702fd3
Philippe Le Quoc <philippe.lequoc@gmail.com>
2024-03-18 13:08:17 +0100

    first commit. Initialize project Record

---
+++ src/main/java/com/recordbackend/RecordBackendApplication.java
@@ -0,0 +1,13 @@
+package com.recordbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/recordbackend/RecordBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recordbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

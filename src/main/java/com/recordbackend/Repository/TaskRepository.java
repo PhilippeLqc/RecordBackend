ac74586cfdf79a80760de04a3b98a6c6cd431076
Arnaud <arnaud.chane.dev@gmail.com>
2024-03-21 14:39:34 +0100

    Remove the query find by id

--- src/main/java/com/recordbackend/Repository/TaskRepository.java
+++ src/main/java/com/recordbackend/Repository/TaskRepository.java
@@ -8,7 +8,4 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
-    @Query("SELECT t FROM Task t WHERE t.id IN :taskIds")
-    List<Task> findAllById(List<Long> taskIds);
 }

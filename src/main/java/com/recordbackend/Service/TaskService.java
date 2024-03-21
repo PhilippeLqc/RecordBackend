8f4b4f8cf7e6312c655e8a561d7c8581e57d5ca1
Arnaud <arnaud.chane.dev@gmail.com>
2024-03-21 14:40:08 +0100

    Task converter, getAll and getTaskById

--- src/main/java/com/recordbackend/Service/TaskService.java
+++ src/main/java/com/recordbackend/Service/TaskService.java
@@ -1,4 +1,60 @@
 package com.recordbackend.Service;
 
+import com.recordbackend.Dto.TaskDto;
+import com.recordbackend.Model.Task;
+import com.recordbackend.Repository.TaskRepository;
+import com.recordbackend.Repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class TaskService {
+
+    private final TaskRepository taskRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository, UserRepository userRepository){
+        this.taskRepository = taskRepository;
+        this.userRepository = userRepository;
+    }
+
+    public List<Task> getAllTask(){
+        return this.taskRepository.findAll();
+    }
+
+    public TaskDto getTaskById(Long id){
+        return this.convertToTaskDto(taskRepository.findById(id).orElseThrow(()-> new EntityNotFoundException("Task not found")));
+    }
+
+
+
+
+
+
+    // Convert to TaskDto
+    public TaskDto convertToTaskDto(Task task){
+        return TaskDto.builder()
+                .title(task.getTitle())
+                .description(task.getDescription())
+                .expirationDate(task.getExpirationDate())
+                .status(task.getStatus())
+                .build();
+    }
+
+    // Convert to Task Entity
+    public Task convertToTaskEntity(TaskDto taskDto){
+        return Task.builder()
+                .title(taskDto.getTitle())
+                .description(taskDto.getDescription())
+                .expirationDate(taskDto.getExpirationDate())
+                .status(taskDto.getStatus())
+                .build();
+    }
+
 }

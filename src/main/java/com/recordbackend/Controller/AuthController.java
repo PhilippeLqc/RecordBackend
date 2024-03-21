7a5c3d4a1fd582ad19cefc1df1d2597564d09fcd
Philippe Le Quoc <philippe.lequoc@gmail.com>
2024-03-21 15:25:20 +0100

    fix minor bug

--- src/main/java/com/recordbackend/Controller/AuthController.java
+++ src/main/java/com/recordbackend/Controller/AuthController.java
@@ -4,17 +4,20 @@ import com.recordbackend.Dto.UserDto;
 import com.recordbackend.Dto.UserRegisterDto;
 import com.recordbackend.Service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
 @AllArgsConstructor
-@RequestMapping("/user")
-public class UserController {
+public class AuthController {
+
     private final UserService userService;
 
-
+    @PostMapping("/register")
+    public UserDto saveUser(UserRegisterDto userRegisterDto){
+        return userService.createUser(userRegisterDto);
+    }
 
 }

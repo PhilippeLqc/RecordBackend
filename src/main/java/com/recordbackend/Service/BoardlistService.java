90d2e03ec2ac24c4a9a37ff4f04b7585a123d29a
Arnaud <arnaud.chane.dev@gmail.com>
2024-03-22 09:55:08 +0100

    Boardlist Service

--- src/main/java/com/recordbackend/Service/BoardlistService.java
+++ src/main/java/com/recordbackend/Service/BoardlistService.java
@@ -1,9 +1,12 @@
 package com.recordbackend.Service;
 
 import com.recordbackend.Dto.BoardlistDto;
+import com.recordbackend.Dto.UserDto;
 import com.recordbackend.Model.Boardlist;
+import com.recordbackend.Model.User;
 import com.recordbackend.Repository.BoardListRepository;
 import com.recordbackend.Repository.ProjectRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,7 @@ public class BoardlistService {
 
     private final BoardListRepository boardListRepository;
     private final ProjectRepository projectRepository;
+    private final ProjectService projectService;
 
 
     // Convert Boardlist to BoardlistDto
@@ -36,14 +40,38 @@ public class BoardlistService {
                 .build();
     }
 
-    public List<BoardlistDto> getAllBoardlist() {
-
+    //------------------------------------------------------------------------------------------------------------
+    //
+    // create a new user
+    public BoardlistDto createBoardlist(BoardlistDto boardlistDto){
+        return this.convertToBoardlistDto(this.boardListRepository.save(this.convertToBoardlistEntity(boardlistDto)));
     }
 
+    // get all Boardlist
+    public List<BoardlistDto> getAllBoardlist() {
+        return this.boardListRepository.findAll().stream().map(this::convertToBoardlistDto).toList();
+    }
 
+    // get boardlistDto by id
+    public BoardlistDto getBoardlistDtoById(Long id){
+        return this.convertToBoardlistDto(this.getBoardlistById(id));
+    }
 
+    // get boardlist entity by id
+    public Boardlist getBoardlistById(Long id){
+        return this.boardListRepository.findById(id).orElseThrow(()-> new EntityNotFoundException("Boardlist not found"));
+    }
 
+    // update boardlist by id
+    public BoardlistDto updateBoardlistById(Long id, BoardlistDto boardlistDto) {
+        Boardlist boardlist = boardListRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Boardlist not found"));
+        boardlist.setName(boardlistDto.getName());
+        boardlist.setProject(this.projectService.findById(boardlistDto.getProjectId()));
+        return convertToBoardlistDto(boardListRepository.save(boardlist));
+    }
 
-
-
+    // delete boardlist by id
+    public void deleteTask(Long id){
+        this.boardListRepository.deleteById(id);
+    }
 }

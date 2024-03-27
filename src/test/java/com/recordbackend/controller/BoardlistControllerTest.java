fe28f79098ae8fcb3ed1d2ed3279827ebdbffec0
Arnaud <arnaud.chane.dev@gmail.com>
2024-03-27 11:47:01 +0100

    Test create boardlist done

--- src/test/java/com/recordbackend/controller/BoardlistControllerTest.java
+++ src/test/java/com/recordbackend/controller/BoardlistControllerTest.java
@@ -1,23 +1,47 @@
 package com.recordbackend.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.recordbackend.Dto.BoardlistDto;
 import com.recordbackend.Service.BoardlistService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class BoardlistControllerTest {
 
-    private final MockMvc mockMvc;
-    public BoardlistControllerTest(MockMvc mockMvc) {
-        this.mockMvc = mockMvc;
-    }
+    @Autowired
+    private MockMvc mockMvc;
 
     @MockBean
     private BoardlistService boardlistService;
 
+    private ObjectMapper objectMapper = new ObjectMapper();
 
+    @Test
+    public void testCreateNewBoardlistWithValidInput() throws Exception {
+        // Arrange
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0M0BtYWlsLmNvbSIsImlhdCI6MTcxMTQ0NzUwMSwiZXhwIjoxNzEyMDUyMzAxfQ.2b0uXtJjhihp7wfk2iJoi5PqCb0fWjgcmOydY7Soob0"; // Replace with your actual token
+        BoardlistDto boardlistDto = new BoardlistDto("Test Boardlist", 102L);
+        when(boardlistService.createBoardlist(any(BoardlistDto.class))).thenReturn(boardlistDto);
 
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.post("/boardlist/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + token) // Add the Authorization header with the token
+                        .content(objectMapper.writeValueAsString(boardlistDto)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Test Boardlist"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.projectId").value(102L));
+    }
 }
